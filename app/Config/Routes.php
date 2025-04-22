582d58465bbd959c38b2a5382a7835b24b6d17a8
19MAX <maxisebas19@gmail.com>
2025-04-22 16:56:30 -0500

    login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+// Rutas públicas
 $routes->get('/', 'Auth\LoginController::index');
+$routes->get('login', 'Auth\LoginController::index');
+$routes->post('auth/login', 'Auth\LoginController::login');
+$routes->get('logout', 'Auth\LoginController::logout');
